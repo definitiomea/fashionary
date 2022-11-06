cd838e39a4f93ad1068e8d998b30bd2c10a67c54
definitiomea <103019360+definitiomea@users.noreply.github.com>
2022-11-06 21:09:12 +0900

    요소 반복 완료, 이제 리액트 슬릭 실험

--- src/Pages/detailfolder/CategoryDetail.jsx
+++ src/Pages/detailfolder/CategoryDetail.jsx
@@ -13,11 +13,6 @@ const CategoryDetail = () => {
         setClothes(data);
     }
 
-    /* const testText = () => {
-        console.log(clothes?.including);
-        console.log(clothes?.including[0].section1[0]);
-    } */
-
     useEffect(() => {
         getClothes();
     },[clothes]);
@@ -26,6 +21,11 @@ const CategoryDetail = () => {
         <>
             <h1>Category Detail Here</h1>
             {clothes && clothes?.including.map((item) => (
+                <div>
+                    <h3>{item && item.type}</h3>
+                </div>
+            ))}
+            {/* {clothes && clothes?.including.map((item) => (
                 <div>
                     <h3>{item && item.type}</h3>
                     <p>{item && item.section1_title}</p>
@@ -47,44 +47,7 @@ const CategoryDetail = () => {
                         ))}
                     </ol>
                 </div>
-            ))}
-            {/* {clothes && clothes?.including.map((item) => (
-                <li>{item && item?.type}
-                    
-                </li>
             ))} */}
-            {/* <hr></hr>
-            <h3>{clothes?.including[0]?.title && clothes?.including[0]?.title}</h3>
-            <p>{clothes?.including[0]?.section1_title && clothes?.including[0]?.section1_title}</p>
-            <ol>
-                <li>{clothes?.including[0]?.section1[0] && clothes?.including[0]?.section1[0]}</li>
-                <li>{clothes?.including[0]?.section1[1] && clothes?.including[0]?.section1[1]}</li>
-                <li>{clothes?.including[0]?.section1[2] && clothes?.including[0]?.section1[2]}</li>
-            </ol>
-            <p>{clothes?.including[0]?.section2_title && clothes?.including[0]?.section2_title}</p>
-            <ol>
-                <li>{clothes?.including[0]?.section2[0] && clothes?.including[0]?.section2[0]}</li>
-                <li>{clothes?.including[0]?.section2[1] && clothes?.including[0]?.section2[1]}</li>
-            </ol>
-            <p>{clothes?.including[0]?.section3_title && clothes?.including[0]?.section3_title}</p>
-            <ol>
-                <li>{clothes?.including[0]?.section3[0] && clothes?.including[0]?.section3[0]}</li>
-                <li>{clothes?.including[0]?.section3[1] && clothes?.including[0]?.section3[1]}</li>
-                <li>{clothes?.including[0]?.section3[2] && clothes?.including[0]?.section3[2]}</li>
-                <li>{clothes?.including[0]?.section3[3] && clothes?.including[0]?.section3[3]}</li>
-                <li>{clothes?.including[0]?.section3[4] && clothes?.including[0]?.section3[4]}</li>
-                <li>{clothes?.including[0]?.section3[5] && clothes?.including[0]?.section3[5]}</li>
-                <li>{clothes?.including[0]?.section3[6] && clothes?.including[0]?.section3[6]}</li>
-                
-            </ol>
-            <ol>
-                <li>{clothes?.including[0]?.section3[7] && clothes?.including[0]?.section3[7]}</li>
-                <li>{clothes?.including[0]?.section3[8] && clothes?.including[0]?.section3[8]}</li>
-                <li>{clothes?.including[0]?.section3[9] && clothes?.including[0]?.section3[9]}</li>
-                <li>{clothes?.including[0]?.section3[10] && clothes?.including[0]?.section3[10]}</li>
-                <li>{clothes?.including[0]?.section3[11] && clothes?.including[0]?.section3[11]}</li>
-                <li>{clothes?.including[0]?.section3[12] && clothes?.including[0]?.section3[12]}</li>
-            </ol> */}
         </>
     );
 }
