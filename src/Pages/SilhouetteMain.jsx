5f28f5f9dc972c1c0d616513dde6b79a0042d0b9
Johnny_P <shinezl39@gmail.com>
2022-11-09 12:12:27 +0900

    실루엣 페이지 뽑으러

--- src/Pages/SilhouetteMain.jsx
+++ src/Pages/SilhouetteMain.jsx
@@ -1,6 +1,30 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Dropdown, DropdownButton } from "react-bootstrap";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const SilhouetteMain = () => {
+    const [silhouette, setSilhouette] = useState(null);
+    const navigate = useNavigate();
+
+    const getSilhouette = async () => {
+        let url = `http://localhost:5000/silhouette/`
+        let response = await fetch(url);
+        let data = await response.json();
+        setSilhouette(data);
+    }
+
+    useEffect(() => {
+        getSilhouette();
+    },[]);
+
+    const dropToMove = () => {
+        const dropMove = [];
+        for(let i = 0; i < silhouette.length; i++) {
+            dropMove.push(<Dropdown.Item onClick={() => {navigate(`/silhouette/${silhouette[i].id}`)}}>{silhouette[i].shape}</Dropdown.Item>);
+        }
+        return dropMove;
+    }
+
     return (
         <>
             <main>
@@ -10,9 +34,18 @@ const SilhouetteMain = () => {
                             <h1 className="visualsub">SILHOUTTE</h1>
                         </div>
                         <Container>
-                            <div className="outlet">
-                                <h1>Silhoutte Main here</h1>
-                            </div>
+                            <Row>
+                                <Col>
+                                    <div className="outlet">
+                                        <div className="dropcontain">
+                                            <DropdownButton id="dropdown-basic-button" title="Choose silhouette Here">
+                                                {silhouette && dropToMove()}
+                                            </DropdownButton>
+                                        </div>
+                                        <Outlet></Outlet>
+                                    </div>
+                                </Col>
+                            </Row>
                         </Container>
                     </section>
                 </article>
