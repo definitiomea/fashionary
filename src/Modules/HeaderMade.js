2f4ec15684e3bec31476dbc7ee05b5da923e1c87
definitiomea <103019360+definitiomea@users.noreply.github.com>
2022-11-09 20:30:45 +0900

    상단 슬라이드

--- src/Modules/HeaderMade.js
+++ src/Modules/HeaderMade.js
@@ -1,9 +1,25 @@
+import React from "react";
 import { useNavigate } from "react-router-dom";
+
+import Slider from "react-slick";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 
 const HeaderMade = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5500,
+        speed: 3000,
+        pauseOnDotsHover: false,
+        pauseOnFocus: false,
+        pauseOnHover: false
+    };
+
     const navigate = useNavigate();
 
     const goHome = () => {
@@ -13,6 +29,11 @@ const HeaderMade = () => {
     return (
         <>
             <header>
+                <Slider {...settings}>
+                    <div className="header-img1"></div>
+                    <div className="header-img2"></div>
+                    <div className="header-img3"></div>
+                </Slider>
                 {/* <div className="home-area"> */}
                     <div className='home-logo' onClick={goHome}>
                         <h1 className="home-title">F<span className="paint-red">A</span>SHION<span className="paint-red">A</span>RY
