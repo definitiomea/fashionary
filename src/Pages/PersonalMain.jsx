e4913752e888e533227d2d8b3a586d86f36dbb37
Johnny_P <shinezl39@gmail.com>
2022-11-11 17:43:40 +0900

    p5 continues......

--- src/Pages/PersonalMain.jsx
+++ src/Pages/PersonalMain.jsx
@@ -6,12 +6,18 @@ const PersonalMain = () => {
     let x = 50;
     let y = 50;
 
+    let backgroundImg;
+
     const setup = (p5, canvasParentRef) => {
         p5.createCanvas(500, 500).parent(canvasParentRef);
+        p5.loadImage("../Sources/Img/192x192.png", img => {
+            p5.image(img, 0, 0);
+            p5.redraw();
+        });
     }
 
     const draw = (p5) => {
-        p5.background(255, 130, 20);
+        p5.background("transparent", "transparent", "transparent");
         p5.ellipse(x, y, 70, 70);
         x++;
     }
@@ -21,10 +27,14 @@ const PersonalMain = () => {
             <main>
                 <article>
                     <section>
+                        <div className="persubvisual">
+                            <h1 className="visualsub">PERSONAL COLOR</h1>
+                        </div>
                         <Container>
                             <div className="outlet">
-                                <h1>PERSONAL COLOR MAIN HERE</h1>
-                                <Sketch setup={setup} draw={draw} />
+                                <div className="percanvas">
+                                    <Sketch setup={setup} draw={draw} />
+                                </div>
                             </div>
                         </Container>
                     </section>
