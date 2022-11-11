d95da0982dcec6cc0d53f117593ed8e4a3aeffab
Johnny_P <shinezl39@gmail.com>
2022-11-11 16:52:26 +0900

    p5 lib..!

--- src/Pages/PersonalMain.jsx
+++ src/Pages/PersonalMain.jsx
@@ -1,6 +1,21 @@
 import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import Sketch from "react-p5";
 
 const PersonalMain = () => {
+    let x = 50;
+    let y = 50;
+
+    const setup = (p5, canvasParentRef) => {
+        p5.createCanvas(500, 500).parent(canvasParentRef);
+    }
+
+    const draw = (p5) => {
+        p5.background(255, 130, 20);
+        p5.ellipse(x, y, 70, 70);
+        x++;
+    }
+
     return (
         <>
             <main>
@@ -9,6 +24,7 @@ const PersonalMain = () => {
                         <Container>
                             <div className="outlet">
                                 <h1>PERSONAL COLOR MAIN HERE</h1>
+                                <Sketch setup={setup} draw={draw} />
                             </div>
                         </Container>
                     </section>
