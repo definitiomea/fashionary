132722e0efdef1d9f8e3d033b83f362ab5f2aa97
definitiomea <103019360+definitiomea@users.noreply.github.com>
2022-11-12 14:28:46 +0900

    css fixed

--- src/Pages/PatternMain.jsx
+++ src/Pages/PatternMain.jsx
@@ -35,7 +35,7 @@ const PatternMain = () => {
                         </div>
                         <Container>
                             <Row>
-                                <Col>
+                                <Col lg={9} style={{margin: "0px auto"}}>
                                     <div className="outlet">
                                         <div className="dropcontain">
                                             <DropdownButton id="dropdown-basic-button" title="Choose pattern Here">
