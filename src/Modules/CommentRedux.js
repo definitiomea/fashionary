9f31ed6ce81ab54fd443d11a3e457848f5dd1f7a
definitiomea <103019360+definitiomea@users.noreply.github.com>
2022-11-14 12:12:43 +0900

    CommentRedux..

--- src/Modules/CommentRedux.js
+++ src/Modules/CommentRedux.js
@@ -1,9 +1,25 @@
-const CommentRedux = () => {
-    return (
-        <>
-            
-        </>
-    );
-}
- 
-export default CommentRedux;
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CommentRedux = createSlice({
+    name: "ManageReply",
+    initialState: {
+        replylist: [
+            {
+                
+            }
+        ]
+    },
+    reducers: {
+        addreply: (state, action) => {
+            let i = 1;
+            const reply = {
+                comment: action.payload.comment,
+                index: i++
+            }
+            state.replylist.concat(reply);
+        },
+        deletereply: (state, action) => {
+            const delReply = state.replylist.filter((reply) => reply.index !== action.index);
+        }
+    }
+});
