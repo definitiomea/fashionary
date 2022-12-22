e8572a83e587fcdc78b2a8837daee9e4d5fc9e9a
definitiomea <103019360+definitiomea@users.noreply.github.com>
2022-12-22 17:26:21 +0900

    privateRoute

--- src/Routes/PrivageRoutes.jsx
+++ src/Routes/PrivageRoutes.jsx
@@ -1,21 +1,25 @@
 import { useSelector } from "react-redux";
-import { Navigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 
-import MyPage from '../Pages/MyPage';
-import PersonalMain from '../Pages/PersonalMain';
+import MyPage from "../Pages/MyPage";
+import PersonalMain from "../Pages/PersonalMain";
 
-export const PrivatePage = ({authenticate}) => {
-    const accountlist = useSelector((state) => state.ManageAccount.accountlist);
+export const PrivatePage = ({ authenticate }) => {
+  const accountlist = useSelector((state) => state.ManageAccount.accountlist);
 
-    return accountlist[0].logined == true ?
-    <MyPage></MyPage> :
+  return accountlist[0].logined == true ? (
+    <MyPage></MyPage>
+  ) : (
     <Navigate to="/login"></Navigate>
-}
+  );
+};
 
-export const PrivateColor = ({authenticate}) => {
-    const accountlist = useSelector((state) => state.ManageAccount.accountlist);
+export const PrivateColor = ({ authenticate }) => {
+  const accountlist = useSelector((state) => state.ManageAccount.accountlist);
 
-    return accountlist[0].logined == true ?
-    <PersonalMain></PersonalMain>:
+  return accountlist[0].logined == true ? (
+    <PersonalMain></PersonalMain>
+  ) : (
     <Navigate to="/login"></Navigate>
-}
+  );
+};
